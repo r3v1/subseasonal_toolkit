14fa980120672b9153774fee1fe2a80d9333d668
David <r3v1@pm.me>
2022-04-29 12:20:18 +0200

    Update Climatology

--- subseasonal_toolkit/models/climatology/batch_predict.py
+++ subseasonal_toolkit/models/climatology/batch_predict.py
@@ -10,42 +10,76 @@
 # Named args:
 #   --target_dates (-t): target dates for batch prediction (default: 'std_test')
 
-import pandas as pd
 from argparse import ArgumentParser
 from datetime import datetime
+
+import matplotlib.pyplot as plt
+import pandas as pd
+from cartopy import crs as ccrs
 from subseasonal_data.data_loaders import get_climatology
-from subseasonal_toolkit.utils.experiments_util import get_id_name, get_th_name, pandas2hdf
+
 from subseasonal_toolkit.utils.eval_util import get_target_dates
+from subseasonal_toolkit.utils.experiments_util import get_id_name, get_th_name
 from subseasonal_toolkit.utils.models_util import save_forecasts
+import xarray as xr
 
-# Load command line arguments
-parser = ArgumentParser()
-parser.add_argument("pos_vars", nargs="*")  # gt_id and horizon
-parser.add_argument('--target_dates', '-t', default="std_test")
-args = parser.parse_args()
+def visualizar_datos(df: pd.DataFrame, variable: str) -> xr.Dataset:
+    # Construímos el dataset
+    ds = df.set_index(["start_date", "lat", "lon"]).to_xarray()
+    da = ds.to_array().isel(variable=0, start_date=0)
 
-# Assign variables
-gt_id = get_id_name(args.pos_vars[0])  # "contest_precip" or "contest_tmp2m"
-horizon = get_th_name(args.pos_vars[1])  # "34w" or "56w"
-target_dates = args.target_dates
+    fig, ax = plt.subplots(
+        1,
+        1,
+        subplot_kw={"projection": ccrs.PlateCarree()},
+        figsize=(10, 5),
+    )
 
-model_name = "climatology"
-submodel_name = "climatology"
+    da.plot(
+        ax=ax,
+        cmap="RdBu",
+        cbar_kwargs=dict(orientation="vertical", fraction=0.0315, pad=0.025),
+    )
+    ax.coastlines()
+    plt.title(f"Extensión de la climatología: {variable}")
+    plt.tight_layout()
+    plt.show()
 
-official_clim = get_climatology(gt_id)
-official_clim["day"] = official_clim.start_date.dt.day
-official_clim["month"] = official_clim.start_date.dt.month
+    return ds
 
-target_date_objs = get_target_dates(date_str=target_dates, horizon=horizon)
-for target_date in target_date_objs:
-    target_date_str = datetime.strftime(target_date, '%Y%m%d')
+if __name__ == "__main__":
 
-    preds = official_clim[(official_clim.day == target_date.day) & (official_clim.month == target_date.month)]
-    preds = preds.drop(["day", "month", "start_date"], axis=1)
-    preds["start_date"] = target_date
-    preds = preds.rename(columns={gt_id.split("_")[1]: 'pred'})
+    # Load command line arguments
+    parser = ArgumentParser()
+    parser.add_argument("pos_vars", nargs="*")  # gt_id and horizon
+    parser.add_argument('--target_dates', '-t', default="std_test")
+    args = parser.parse_args()
 
-    # Save predictions
-    save_forecasts(preds, model=model_name, submodel=submodel_name,
-                   gt_id=gt_id, horizon=horizon,
-                   target_date_str=target_date_str)
+    # Assign variables
+    gt_id = get_id_name(args.pos_vars[0])  # "contest_precip" or "contest_tmp2m"
+    horizon = get_th_name(args.pos_vars[1])  # "34w" or "56w"
+    target_dates = args.target_dates
+
+    model_name = "climatology"
+    submodel_name = "climatology"
+
+    official_clim = get_climatology(gt_id, sync=False, allow_write=True)
+    # ds = visualizar_datos(official_clim, gt_id)  # Para visualizar la extensión
+
+    official_clim["day"] = official_clim.start_date.dt.day
+    official_clim["month"] = official_clim.start_date.dt.month
+
+    target_date_objs = get_target_dates(date_str=target_dates, horizon=horizon)
+    for target_date in target_date_objs:
+        target_date_str = datetime.strftime(target_date, '%Y%m%d')
+
+        # La climatología ya está calculada, simplemente hay que filtrar según el día y el mes objetivo
+        preds = official_clim[(official_clim.day == target_date.day) & (official_clim.month == target_date.month)]
+        preds = preds.drop(["day", "month", "start_date"], axis=1)
+        preds["start_date"] = target_date
+        preds = preds.rename(columns={gt_id.split("_")[1]: 'pred'})
+
+        # Save predictions
+        save_forecasts(preds, model=model_name, submodel=submodel_name,
+                       gt_id=gt_id, horizon=horizon,
+                       target_date_str=target_date_str)
